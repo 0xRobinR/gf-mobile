1e0736e80e5ab9955fc6db54fcd3a3a687526a1c
Robin Rajput <robin.rajput@360coreinc.com>
2023-09-03 23:27:47 +0530

    list user buckets

--- lib/views/my_files/GFFiles.dart
+++ lib/views/my_files/GFFiles.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gf_mobile/components/Text/SubTitle.dart';
+import 'package:gf_mobile/state/FetchUserBuckets.dart';
 import 'package:gf_mobile/views/my_files/components/GFFileTile.dart';
+import 'package:gf_sdk/models/GFBucket.dart';
 
 class GFFiles extends StatefulWidget {
   const GFFiles({super.key});
@@ -10,30 +12,46 @@ class GFFiles extends StatefulWidget {
 }
 
 class _GFFilesState extends State<GFFiles> {
+  List<GFBucket> buckets = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchBuckets();
+  }
+
+  fetchBuckets() async {
+    final fetchedBuckets = await getUserBuckets(
+        address: "0xbb900Eacda882c7c2FA5C1e548D7E7149d31Ccee",
+        spURL: "https://gnfd-testnet-sp1.bnbchain.org");
+
+    setState(() {
+      buckets = fetchedBuckets;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("My Files"),
         centerTitle: true,
-        actions: [IconButton(icon: const Icon(Icons.search), onPressed: () {})],
+        actions: [],
       ),
       body: SafeArea(
           child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Container(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Subtitle(title: "Here are your buckets on Greenfield"),
-                const GFFileTile(
-                  title: "phoenix",
-                ),
-                const GFFileTile(
-                  title: "bnb-bucket",
-                ),
-              ],
-            ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Subtitle(title: "Here are your buckets on Greenfield"),
+              ...buckets
+                  .map((e) => GFFileTile(
+                      title: e.bucketName,
+                      createdAt: e.created * 1000,
+                      block: e.block))
+                  .toList()
+            ],
           ),
         ),
       )),
--- lib/views/my_files/components/GFFileTile.dart
+++ lib/views/my_files/components/GFFileTile.dart
@@ -1,19 +1,33 @@
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
 import 'package:gf_mobile/components/list/GListTile.dart';
 import 'package:gf_mobile/theme/themes.dart';
 
 class GFFileTile extends StatelessWidget {
   final String title;
+  final int createdAt;
+  final int block;
 
-  const GFFileTile({super.key, required this.title});
+  const GFFileTile(
+      {super.key,
+      required this.title,
+      required this.createdAt,
+      required this.block});
 
   @override
   Widget build(BuildContext context) {
+    // format date in dd-MMM-YY h:m a format
     return GListTile(
         index: 0,
         icon: Icon(Icons.folder, color: bnbColor),
         title: title,
-        subtitle: "created on 12/08/2023",
+        subtitle:
+            "created on ${formatDate(DateTime.fromMillisecondsSinceEpoch(createdAt), [
+              d,
+              ' ',
+              M,
+              ' '
+            ])} at block #$block",
         trailingIcon: IconButton(
             icon: const Icon(Icons.more_vert, color: Colors.grey),
             onPressed: () {}));
